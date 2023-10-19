a3bbc6fdc80546b9bf9c1e144118f4cca5ff3e22
Sagar Bharti <sagar.bharti@sapidblue.com>
2023-10-19 14:59:01 +0530

    fix issues

--- webpack.common.js
+++ webpack.common.js
@@ -1,49 +1,139 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+// const outputPath = path.join(__dirname, "dist");
+const extractSass = new ExtractTextPlugin({
+  filename: '[name].[hash].css',
+  disable: process.env.NODE_ENV === 'development',
+});
 module.exports = {
-  entry: {
-    app: "./src/index.js",
-  },
+  mode: 'production',
+  entry: { app: './src/index.js' },
   output: {
-    // filename: "app.js",
-    path: path.resolve(__dirname, "public"),
+    filename: '[name].[hash].js',
+    publicPath: '/'
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        // exclude: /node_modules/,
         use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
           {
-            loader: "postcss-loader",
+            loader: 'file-loader',
             options: {
-              postcssOptions: {
-                plugins: [
-                  [
-                    "postcss-preset-env",
-                    {
-                      // Options
-                    },
-                  ],
-                ],
+              name: "[path]/[name].[ext]",
+              outputPath: './assets/img/',
+              context: 'src/assets/img/',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 70,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: '70-90',
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
               },
             },
           },
         ],
       },
       {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: "./[name].[ext]",
+          outputPath: './assets/fonts/'
         },
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: extractSass.extract({
+          use: [
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+          // use style-loader in development
+          fallback: 'style-loader',
+        }),
+      },
+      {
+        test: /\.css$/,
+        use: extractSass.extract({
+          fallback: 'style-loader',
+          use: 'css-loader',
+        }),
       },
     ],
   },
-  plugins: [new MiniCssExtractPlugin()],
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true,
+    hot: true
+  },
+  plugins: [
+    extractSass,
+    // new ExtractTextPlugin('[name].[hash].css'),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery"
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+        },
+      },
+    },
+  },
 };
