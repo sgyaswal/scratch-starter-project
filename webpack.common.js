deb5e3ff3575ee9b64feec2a358969318176422b
Sagar Bharti <sagar.bharti@sapidblue.com>
2023-10-19 15:00:26 +0530

    fix issues

--- webpack.common.js
+++ webpack.common.js
@@ -1,139 +1,49 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
-// const outputPath = path.join(__dirname, "dist");
-const extractSass = new ExtractTextPlugin({
-  filename: '[name].[hash].css',
-  disable: process.env.NODE_ENV === 'development',
-});
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
-  mode: 'production',
-  entry: { app: './src/index.js' },
+  entry: {
+    app: "./src/index.js",
+  },
   output: {
-    filename: '[name].[hash].js',
-    publicPath: '/'
+    // filename: "app.js",
+    path: path.resolve(__dirname, "public"),
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
         },
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        // exclude: /node_modules/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: "[path]/[name].[ext]",
-              outputPath: './assets/img/',
-              context: 'src/assets/img/',
-            },
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 70,
-              },
-              // optipng.enabled: false will disable optipng
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: '70-90',
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              // the webp option will enable WEBP
-              webp: {
-                quality: 75,
-              },
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: "./[name].[ext]",
-          outputPath: './assets/fonts/'
-        },
-      },
-      {
-        test: /\.html$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: { minimize: true },
-          },
-        ],
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: extractSass.extract({
-          use: [
-            {
-              loader: 'css-loader',
-            },
-            {
-              loader: 'sass-loader',
-            },
-          ],
-          // use style-loader in development
-          fallback: 'style-loader',
-        }),
-      },
-      {
-        test: /\.css$/,
-        use: extractSass.extract({
-          fallback: 'style-loader',
-          use: 'css-loader',
-        }),
       },
     ],
   },
-  devServer: {
-    contentBase: './dist',
-    historyApiFallback: true,
-    hot: true
-  },
-  plugins: [
-    extractSass,
-    // new ExtractTextPlugin('[name].[hash].css'),
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    }),
-    new CleanWebpackPlugin(),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      "window.jQuery": "jquery"
-    })
-  ],
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          chunks: 'all',
-        },
-      },
-    },
-  },
+  plugins: [new MiniCssExtractPlugin()],
 };
